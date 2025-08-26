bc76cd0bad55712a2162071d1ef1dc00dd7697dd
Sharon <sharonirungu2006@gmail.com>
2025-08-26 10:21:08 +0300

    done

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,16 @@
 class MySet:
+    def __init__(self, items=[]):
+        self.dictionary = {}
+        for value in items:
+            self.dictionary[value] = True
 
-    pass
+    def has(self, value):
+      return value in self.dictionary
+
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
+
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self
